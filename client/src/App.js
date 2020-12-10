729c9f46d0a76023216e276ea32df7ca8eb9a60c
miguelmorais7 <42868106+miguelmorais7@users.noreply.github.com>
2020-12-10 14:16:45 -0300

    actions/reducers update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts.js';
 import memories from './images/memories.png'
 import Posts from './Components/Posts/Posts.js'
 import Form from './Components/Form/Form.js'
@@ -8,6 +11,10 @@ import useStyles from './styles.js'
 
 const App = () =>{
     const classes = useStyles();
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getPosts());
+    },[dispatch]);
     return(
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = "mongodb+srv://test:123@cluster0.baeol.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
